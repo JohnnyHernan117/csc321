2e67c4f2fffca9757478e0c1b75bb39cbad077d1
jh <johnnyhernan117@gmail.com>
2025-02-26 01:59:02 +0000

    lab7

---
+++ jhernandez7.py
@@ -0,0 +1,30 @@
+#Johnny Hernandez
+#lab 7
+#CSC 321
+
+x = 7
+y = 5.5
+z = "Mississippi"
+
+if x > 8:
+    print("x is greater than 8")
+elif x < 6:
+    print("x is less than 6")
+elif x == 7:
+    print("x is equal to 7")
+else:
+    print("x is between 6 and 8")
+
+if z != "Mississippi":
+    print("variable z is not \"Mississippi\"")
+else:
+    print("variable z is \"Mississippi\"")
+
+if y == 5.5:
+    print("y is equal to 5.5")
+elif y < 4.5:
+    print("y is less than 4.5")
+elif y > 6.5:
+    print("y is greater than 6.5")
+else:
+    print("y is between 4.5 and 6.5")
